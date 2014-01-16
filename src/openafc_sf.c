1dbbeacf1f981145968bec2b9a5990ecf72960eb
3x7R00Tripper <yoda.kremer@gmail.com>
2014-01-16 22:07:53 +0100

    Update openafc_sf.c

--- src/openafc_sf.c
+++ src/openafc_sf.c
@@ -18,7 +18,7 @@
 #include <libimobiledevice/installation_proxy.h>
 
 #define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
 #define KRED  "\x1B[31m"
 #define KWHT  "\x1B[37m"
 
